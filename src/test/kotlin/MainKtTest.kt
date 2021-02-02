fadaf57846d93760d31941e2cd93d34a89c82eda
Marina Fedkova <mzaloznaya@ya.ru>
2021-02-02 09:42:25 +0300

    bug fixes

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -6,7 +6,7 @@ class MainKtTest {
     @Test
     fun add() {
         val post = Post(id = 0)
-        val expected = 1
+        val expected = 4
         WallService.add(post)
         val result = WallService.add(post).id
 
@@ -16,7 +16,7 @@ class MainKtTest {
     @Test
     fun update_true() {
         val post1 = Post(id = 11)
-        val post2 = Post(id = 0, text = "newText")
+        val post2 = Post(id = 1, text = "newText")
         WallService.add(post1)
 
         val result = WallService.update(post2)
@@ -27,7 +27,7 @@ class MainKtTest {
     @Test
     fun update_false() {
         val post1 = Post(id = 11)
-        val post2 = Post(id = 12, text = "newText")
+        val post2 = Post(id = 2, text = "newText")
         WallService.add(post1)
 
         val result = WallService.update(post2)
