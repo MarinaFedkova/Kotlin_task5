8b2e5d5352f604e0817d07d82802d37b1c51d4c8
Marina Fedkova <mzaloznaya@ya.ru>
2021-02-03 15:42:51 +0300

    rework

--- src/main/kotlin/Event.kt
+++ src/main/kotlin/Event.kt
@@ -1,7 +1,5 @@
 class Event (
-    override val type:String,
     val id: Int? = null,
     val time: Int? = null,
     val address: String? = null
-        ): Attachment {
-}
+        )
--- src/main/kotlin/Sticker.kt
+++ src/main/kotlin/Sticker.kt
@@ -1,6 +1,4 @@
 class Sticker (
-    override val type: String,
     val stickerId: Int? = null,
     val isAllowed: Boolean? = null
-        ): Attachment {
-}
+        )
